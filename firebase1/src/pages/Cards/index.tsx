3ea35e0c519806ffdb949670fadb26a351c46d18
saranya-2303 <saranyasriram23@gmail.com>
2024-10-30 01:38:14 +0530

    firebase

---
+++ firebase1/src/pages/Cards/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FoodDetails from '@/Components/cards/Fooddetails'
+import Header from '@/Components/Subcomponents/Demoheader'
+ 
+const index = () => {
+  return (
+    <div className='p-1 h-screen'>
+      <Header/>
+        <FoodDetails/>
+       
+    </div>
+  )
+}
+ 
+export default index
